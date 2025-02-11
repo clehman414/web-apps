f75839651b7dbf99d0b9576b9701e28b9aace3c1
Caitlin Lehman <caitlin.lehman@kellogg.northwestern.edu>
2025-02-11 21:40:43 +0000

    Creates dice website

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,5 +1,6 @@
 class TacosController < ApplicationController
   def index
-    render :inline => "<h1>Hello, world!</hi>" 
+    #render :inline => "<h1>Hello, world!</hi>" 
+    render :template => "tacos/index"
   end
 end
